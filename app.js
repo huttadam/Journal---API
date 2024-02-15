74aaa071d404fb4e3c31848cd1ef7944e2556458
huttadam <huttjamesadam@gmail.com>
2024-02-15 11:13:56 +1100

    fixed express import

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { express } from 'express'
+import express from 'express'
 import categoryRoutes from './routes/category_routes.js'
 import entryRoutes from './routes/entry_routes.js'
 import cors from 'cors'
